97cc5045fe41492f9ba2d375bd2f6d8a132f9a86
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-05-26 00:14:26 +0530

    button

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <Button onPress={() => alert("button")} title="presse me" />
+        <Button
+          onPress={() => alert("button")}
+          title="presse me"
+          color="#f194ff"
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  body: {
+    width: "60%",
+    height: "10%",
+    justifyContent: "space-between",
   },
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# React Native Button
+
+## Usage
+
+```
+ $ git clone https://github.com/SuryaKarmakar/React-Native-Button.git
+ $ cd React-Native-Button
+ $ npm install
+ $ npm run [web, android, ios]
+
+```
+
+## Run with Expo
+
+```
+ $ expo start --[web, android, ios]
+
+```
