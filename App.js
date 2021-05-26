24a5ec3da474979d954bd86ef0cc23c65e496f38
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-05-26 13:01:31 +0530

    using button

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [disabled, setDisabled] = useState(true);
   return (
     <View style={styles.container}>
       <View style={styles.body}>
+        {/*React native default button*/}
         <Button onPress={() => alert("button")} title="presse me" />
+        {/*React native custom buttom with color*/}
         <Button
           onPress={() => alert("button")}
           title="presse me"
           color="#f194ff"
+        />
+      </View>
+      {/*use disabled props with react hooks*/}
+      <View style={styles.body_row}>
+        <Button
+          onPress={() => alert("now i am free")}
+          title="button 1"
+          disabled={disabled}
+        />
+        <Button
+          onPress={() => alert("button")}
+          title="enable button 1"
+          onPress={() => setDisabled(false)}
         />
       </View>
     </View>
@@ -26,5 +42,12 @@ const styles = StyleSheet.create({
     width: "60%",
     height: "10%",
     justifyContent: "space-between",
+  },
+  body_row: {
+    width: "60%",
+    height: "5%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
   },
 });
--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # React Native Button
 
+## Basic Button :
+
+<p>A basic button component that should render nicely on any platform. Supports a minimal level of customization.in basic button <b>onPress</b> and <b>title</b> both props are <b>Required</b></p>
+
+```js
+<Button onPress={() => alert("button")} title="presse me" />
+```
+
+## Props :
+
+# onPress :
+
+<p>Handler to be called when the user taps the button.</p>
+
+```js
+<Button onPress={() => alert("button")} title="presse me" color="#f194ff" />
+```
+
+# title :
+
+<p>Text to display inside the button. On Android the given title will be converted to the uppercased form.</p>
+
+```js
+title = "presse me";
+```
+
+# color :
+
+<p>This props change the button color</p>
+
+```js
+color = "#f194ff";
+```
+
+# disabled :
+
+<p>If true, disable all interactions for this component.it disable the onPress handler</p>
+
+```js
+const [disabled, setDisabled] = useState(true);
+...
+<View style={styles.body_row}>
+  <Button
+    onPress={() => alert("now i am free")}
+    title="button 1"
+    disabled={disabled}
+  />
+  <Button
+    onPress={() => alert("button")}
+    title="enable button 1"
+    onPress={() => setDisabled(false)}
+  />
+</View>
+...
+```
+
 ## Usage
 
 ```
